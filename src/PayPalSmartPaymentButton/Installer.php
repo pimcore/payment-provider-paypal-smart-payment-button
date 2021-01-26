60c6e9a428e7f84aa2c16588304e176b44f327f6
dvesh3 <divesh.pahuja@pimcore.com>
2021-01-26 19:32:56 +0100

    initial commit

---
+++ src/PayPalSmartPaymentButton/Installer.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Enterprise License (PEL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PEL
+ */
+namespace Pimcore\Bundle\EcommerceFrameworkBundle\PayPalSmartPaymentButton;
+
+use Pimcore\Bundle\EcommerceFrameworkBundle\Tools\PaymentProviderInstaller;
+
+class Installer extends PaymentProviderInstaller
+{
+
+    protected $bricksToInstall = [
+        'PaymentProviderPayPal' => 'objectbrick_PaymentProviderPayPalSmartButton_export.json'
+    ];
+}
